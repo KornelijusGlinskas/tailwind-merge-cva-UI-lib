17dbd20d2a9e2406f291df7e0d2ae922eecaf56f
kornelijusglinskas <kornelijusglinskas8@gmail.com>
2024-11-12 17:49:41 +0200

    Fix TS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   const [selectedName, setSelectedName] = useState("");
   const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!selectedName) {
